bcf1264e1165f5bf71159db17117051e0b54e000
yuto <mr.yuto.pia@gmail.com>
2018-12-24 01:17:15 +0900

    余分な出力を削除する

--- hooks/install.sh
+++ hooks/install.sh
@@ -3,9 +3,9 @@
 source /home/centos/.bashrc
 
 cd /home/centos/repos/kniveout-discord-bot
-nvm use v11.5.0 >& /home/centos/nvmlog.txt
-/home/centos/.yarn/bin/yarn install >& /home/centos/yarn_installlog.txt
-/home/centos/.yarn/bin/yarn build >& /home/centos/yarn_buildlog.txt
+nvm use v11.5.0
+/home/centos/.yarn/bin/yarn install
+/home/centos/.yarn/bin/yarn build
 
 sudo cp ./hooks/kniveout-discord-bot.service /etc/systemd/system/
 sudo /usr/bin/systemctl enable kniveout-discord-bot
