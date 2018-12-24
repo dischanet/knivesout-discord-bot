dcbab2aaea3b4866450fa7b8c71de08f242f2240
yuto <mr.yuto.pia@gmail.com>
2018-12-24 18:33:42 +0900

    environment作成をinstall.shでやる

--- hooks/install.sh
+++ hooks/install.sh
@@ -2,6 +2,10 @@
 
 cd /home/centos/repos/knivesout-discord-bot
 
-sudo ./hooks/set-environment.sh
+ZONE=$(curl 169.254.169.254/latest/meta-data/placement/availability-zone)
+REGION=$(echo ${ZONE/%?/})
+PARAMETER_NAME=KNIVESOUT_DISCORD_BOT_SECRET
+echo "DISCORD_BOT_TOKEN=$(aws --region ${REGION} ssm get-parameters --name ${PARAMETER_NAME} --query "Parameters[0].Value" --output text)" > environment
+
 sudo cp ./hooks/knivesout-discord-bot.service /etc/systemd/system/knivesout-discord-bot.service
 sudo /usr/bin/systemctl enable knivesout-discord-bot
--- hooks/set-environment.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source /home/centos/.bash_profile
-source /home/centos/.bashrc
-
-ZONE=$(curl 169.254.169.254/latest/meta-data/placement/availability-zone)
-REGION=$(echo ${ZONE/%?/})
-PARAMETER_NAME=KNIVESOUT_DISCORD_BOT_SECRET
-echo "DISCORD_BOT_TOKEN=$(aws --region ${REGION} ssm get-parameters --name ${PARAMETER_NAME} --query "Parameters[0].Value" --output text)" > environment
