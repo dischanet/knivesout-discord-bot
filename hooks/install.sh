60afa1db41315c1c210c1864292a1c78894904a2
yuto <mr.yuto.pia@gmail.com>
2018-12-24 18:43:16 +0900

    aws commandをフルパスにする

--- hooks/install.sh
+++ hooks/install.sh
@@ -5,7 +5,8 @@ cd /home/centos/repos/knivesout-discord-bot
 ZONE=$(curl 169.254.169.254/latest/meta-data/placement/availability-zone)
 REGION=$(echo ${ZONE/%?/})
 PARAMETER_NAME=KNIVESOUT_DISCORD_BOT_SECRET
-echo "DISCORD_BOT_TOKEN=$(aws --region ${REGION} ssm get-parameters --name ${PARAMETER_NAME} --query "Parameters[0].Value" --output text)" > environment
+
+echo "DISCORD_BOT_TOKEN=$(/home/centos/.pyenv/shims/aws --region ${REGION} ssm get-parameters --name ${PARAMETER_NAME} --query "Parameters[0].Value" --output text)" > environment
 
 sudo cp ./hooks/knivesout-discord-bot.service /etc/systemd/system/knivesout-discord-bot.service
 sudo /usr/bin/systemctl enable knivesout-discord-bot
