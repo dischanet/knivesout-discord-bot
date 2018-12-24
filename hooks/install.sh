467a9b9b0ad52e557c844de3aafff082abb41ca5
yuto <mr.yuto.pia@gmail.com>
2018-12-24 16:26:52 +0900

    トークンの取得をsystemdではなくhooksで行う

--- hooks/install.sh
+++ hooks/install.sh
@@ -2,5 +2,6 @@
 
 cd /home/centos/repos/kniveout-discord-bot
 
+sudo ./hooks/set-environment.sh
 sudo cp ./hooks/kniveout-discord-bot.service /etc/systemd/system/
 sudo /usr/bin/systemctl enable kniveout-discord-bot
