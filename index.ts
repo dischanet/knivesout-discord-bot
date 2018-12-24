d6db3e18c870c0e511017351d635db3bb53d86fe
yuto <mr.yuto.pia@gmail.com>
2018-12-24 16:09:29 +0900

    Discord bot tokenをaws-sdkではなくaws cliで取得する

--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
 import Discord from 'discord.js'
 import process from 'process'
-import SSM from 'aws-sdk'
 
 const client = new Discord.Client()
 
@@ -14,11 +13,4 @@ client.on('message', msg => {
   }
 })
 
-const ssm = new SSM.SSM({ region: 'ap-northeast-1' })
-
-ssm.getParameter({ Name : 'KNIVESOUT_DISCORD_BOT_SECRET' }, (err, data) => {
-  if (err) {
-    console.log(err, err.stack)
-  }else if(data.Parameter)
-    client.login(data.Parameter["Value"])
-})
+client.login(process.env["DISCORD_BOT_TOKEN"])
