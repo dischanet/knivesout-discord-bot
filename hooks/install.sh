4d5e58c00bfb0574670dfe666ff9f4a8fd0f8f91
yuto <mr.yuto.pia@gmail.com>
2018-12-24 14:27:00 +0900

    CodePipelineでビルドできるように設定する

--- hooks/install.sh
+++ hooks/install.sh
@@ -3,9 +3,6 @@
 source /home/centos/.bashrc
 
 cd /home/centos/repos/kniveout-discord-bot
-nvm use v11.5.0
-/home/centos/.yarn/bin/yarn install
-/home/centos/.yarn/bin/yarn build
 
 sudo cp ./hooks/kniveout-discord-bot.service /etc/systemd/system/
 sudo /usr/bin/systemctl enable kniveout-discord-bot
