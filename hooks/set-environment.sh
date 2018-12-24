4c451de7a71fa3864a77ba81341be1e93d3aa90b
yuto <mr.yuto.pia@gmail.com>
2018-12-24 16:31:53 +0900

    aws cliを使うためにbashの設定を読み込む

--- hooks/set-environment.sh
+++ hooks/set-environment.sh
@@ -1,3 +1,6 @@
+source /home/centos/.bash_profile
+source /home/centos/.bashrc
+
 ZONE=$(curl 169.254.169.254/latest/meta-data/placement/availability-zone)
 REGION=$(echo ${ZONE/%?/})
 PARAMETER_NAME=KNIVESOUT_DISCORD_BOT_SECRET
