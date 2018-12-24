db30d043a5d27f2b776c5376b2ddf54d96dece3c
yuto <mr.yuto.pia@gmail.com>
2018-12-24 16:50:09 +0900

    unitファイルをシンボリックリンクにする

--- hooks/install.sh
+++ hooks/install.sh
@@ -3,5 +3,5 @@
 cd /home/centos/repos/knivesout-discord-bot
 
 sudo ./hooks/set-environment.sh
-sudo cp ./hooks/knivesout-discord-bot.service /etc/systemd/system/
+sudo ln -s ./hooks/knivesout-discord-bot.service /etc/systemd/system/knivesout-discord-bot.service
 sudo /usr/bin/systemctl enable knivesout-discord-bot
